df2759f02d91a9af62452fbd03be8892fbc01351
Dr. Gernot Starke <gs@gernotstarke.de>
2020-12-31 14:03:48 +0100

    added some comment, reducing number of linter errors

--- samplepdfs/samplePDFs.go
+++ samplepdfs/samplePDFs.go
@@ -5,8 +5,15 @@ package samplepdfs
 // private constant
 
 // public constants
+
+// DirPrefix is the relative path to the directory containing samples
 const DirPrefix = "../SamplePDFs/"
+
+// OnePageFile has exactly one page
 const OnePageFile = DirPrefix + "sample-A4-portrait-1pg.pdf"
+
+// ThreePageFile has exactly three pages
 const ThreePageFile = DirPrefix + "sample-A4-portrait-3pg.pdf"
 
+// A file with PDF extension, but markdown content
 const DisguisedMarkdownFile = DirPrefix + "md-disguised-as.pdf"
