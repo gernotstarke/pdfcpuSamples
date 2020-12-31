d82b3349d4a300453c7c22de3dc0b63f81e4dca3
Dr. Gernot Starke <gs@gernotstarke.de>
2020-12-31 15:08:42 +0100

    use better test data

--- information/pageCount_test.go
+++ information/pageCount_test.go
@@ -35,17 +35,16 @@ func TestPageCount(t *testing.T) {
 		t.Errorf("FAIL: CountPagesOfPDFFile()  %v, expected %v pages, got %v", samplePDFs.ThreePageFile, 3, got)
 	}
 
-	/*
-		for _, f := range testPDFs {
-			got, err := CountPagesOfPDFFile(f.fileName)
+	for _, f := range testPDFs {
+		got, err := CountPagesOfPDFFile(f.fileName)
 
-			if (err == nil) != f.expectedError {
-				t.Errorf("FAIL: file %s should yield error, but didnt", f.fileName)
-			}
-
-			if got != f.expectedNrOfPages {
-				t.Errorf("FAIL: file %s expected %v pages, got %v", f.fileName, f.expectedNrOfPages, got)
-			}
+		if (err == nil) == f.expectedError {
+			t.Errorf("FAIL: file %s should yield error, but didnt", f.fileName)
 		}
-	*/
+
+		if got != f.expectedNrOfPages {
+			t.Errorf("FAIL: file %s expected %v pages, got %v", f.fileName, f.expectedNrOfPages, got)
+		}
+	}
+
 }
