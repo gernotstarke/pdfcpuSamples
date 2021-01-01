ead9162a22d8c983a65cd0659d5e1cbcffb6cfc2
Dr. Gernot Starke <gs@gernotstarke.de>
2021-01-01 15:32:15 +0100

    package comment in "doc.go"

---
+++ information/doc.go
@@ -0,0 +1,17 @@
+/*
+Package information contains functions to determine several attributes of
+PDF files and directories containing such files.
+*/
+package information
+
+/*
+These functions include:
+
+* validate if a file exists and contains PDF-compliant content
+* determine the number of pages within a PDF
+* determine the page size(s) of a PDF file
+* count the number of PDF files within a directory
+* collect the names of all PDF files within a directory
+
+
+*/
